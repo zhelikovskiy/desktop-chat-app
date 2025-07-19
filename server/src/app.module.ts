851dacdb31baa13625b40fc2a34aef224d382255
Artem Zhelikovskyi <zhelikovskiy.artem@gmail.com>
2025-07-19 11:25:46 +0200

    server: add prisma module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { UserService } from './user/user.service';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-	imports: [UsersModule, AuthModule],
+	imports: [UsersModule, AuthModule, PrismaModule],
 	controllers: [AppController],
 	providers: [AppService, UserService],
 })
