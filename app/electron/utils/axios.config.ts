74429b136e55e672cc9466c94c862ad1637ffbf1
Artem Zhelikovskyi <zhelikovskiy.artem@gmail.com>
2025-07-22 14:55:07 +0200

    app: add base auth, fix bugs, add pinia, add electron store for tokens

---
+++ app/electron/utils/axios.config.ts
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { getAccessToken, getRefreshToken, setAccessToken } from './store';
+
+const api = axios.create({
+	baseURL: 'http://localhost:3000',
+	timeout: 5000,
+	headers: {
+		'Content-Type': 'application/json',
+		Accept: 'application/json',
+	},
+});
+
+api.interceptors.request.use((config) => {
+	const token = getAccessToken();
+	if (token) {
+		config.headers.Authorization = `Bearer ${token}`;
+	}
+	return config;
+});
+
+api.interceptors.response.use(
+	(res) => res,
+	async (error) => {
+		const originalRequest = error.config;
+		const isAuthEndpoint =
+			originalRequest.url?.includes('/auth/login') ||
+			originalRequest.url?.includes('/auth/refresh') ||
+			originalRequest.url?.includes('/auth/register');
+
+		if (isAuthEndpoint) {
+			return Promise.reject(error);
+		}
+
+		if (error.response?.status === 401 && !originalRequest._retry) {
+			originalRequest._retry = true;
+			try {
+				const refreshToken = getRefreshToken();
+				const response = await api.post('/auth/refresh', {
+					refreshToken: refreshToken,
+				});
+
+				if (response.data.accessToken)
+					setAccessToken(response.data.accessToken);
+
+				return api(originalRequest);
+			} catch (error) {
+				return Promise.reject(error);
+			}
+		}
+		return Promise.reject(error);
+	}
+);
+
+export default api;
--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -1,11 +1,38 @@
 import { createMemoryHistory, createRouter } from 'vue-router';
+import { useAuthStore } from '@/stores/auth.store';
 import HomeView from '@/views/HomeView.vue';
 
-const routes = [{ path: '/', component: HomeView }];
+const routes = [
+	{ path: '/', component: HomeView, meta: { requiresAuth: true } },
+	{
+		path: '/register',
+		component: () => import('@/views/auth/RegisterView.vue'),
+		meta: { public: true },
+	},
+	{
+		path: '/login',
+		component: () => import('@/views/auth/LoginView.vue'),
+		meta: { public: true },
+	},
+];
 
 const router = createRouter({
 	history: createMemoryHistory(),
 	routes,
+});
+
+router.beforeEach((to, from, next) => {
+	const authStore = useAuthStore();
+	if (
+		(to.path === '/login' || to.path === '/register') &&
+		authStore.isAuthenticated
+	) {
+		next('/');
+	} else if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+		next('/login');
+	} else {
+		next();
+	}
 });
 
 export default router;
